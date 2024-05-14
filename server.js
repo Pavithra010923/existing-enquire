39242fc0d1b495f65c9203c2edb7fc3dbb5cdafe
Pavithra010923 <selvapavithra45@gmail.com>
2024-05-14 12:06:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/send-email', (req, res) => {
+    const { name, email, message } = req.body;
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'selvapavithra01@gmail.com',
+            pass: 'mprx byub qjom klei' // Replace 'your-password' with your actual Gmail password
+        }
+    });
+
+    let mailOptions = {
+        from: email,
+        to: 'selvapavithra01@gmail.com', 
+        subject: 'New Message from Contact Form',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}` // Use backticks for template literals
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        res.redirect('/');
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
